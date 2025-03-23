19f89d7edc391a7947040f5cf5f6d0a9d6b7ff42
Rasoir-genereux <kevintonou@icloud.com>
2025-03-23 18:38:29 -0400

    Initial commit to the dev1 branch

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #Je suis KTO et connu sous le nom de @KTOBOTZ.
 #Je collabore aujourd'hui avec mon formateur Yves Gouverneur et @Rasoir-genereux sur un projet top secret.
 
-#C'est @Rasoir-genereux. Je suis le collègue de KTO et je suis ravi de suivre le cours F66.
+#Un dépôt dérivé, ce n’est pas simple.
