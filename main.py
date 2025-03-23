f104b0dd5d7e29555183702574c7a55616c3423b
Rasoir-genereux <kevintonou@icloud.com>
2025-03-23 15:25:28 -0400

    @Rasoir-gen. correcting his first commit message

--- main.py
+++ main.py
@@ -1,4 +1,4 @@
 #Je suis KTO et connu sous le nom de @KTOBOTZ.
 #Je collabore aujourd'hui avec mon formateur Yves Gouverneur et @Rasoir-genereux sur un projet top secret.
 
-#C'est @Rasoir-genereux. Je suis le formateur de KTO et je suis ravi de suivre le cours F66.
+#C'est @Rasoir-genereux. Je suis le collègue de KTO et je suis ravi de suivre le cours F66.
